399db7334e9c9d53be08f38289059bed1e006127
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2019-04-25 08:13:41 -0500

    add acript that strips discritics

---
+++ examples/toascii.sh
@@ -0,0 +1,1 @@
+sed 's/A/́/g; s/E/́/g; s/I/́/g; s/O/́/g; s/U/́/g; s/a/́/g; s/e/́/g; s/i/́/g; s/o/́/g; s/u/́/g; s/A/̀/g; s/E/̀/g; s/I/̀/g; s/O/̀/g; s/U/̀/g; s/a/̀/g; s/e/̀/g; s/i/̀/g; s/o/̀/g; s/u/̀/g; s/A/́/g; s/E/́/g; s/I/́/g; s/O/́/g; s/U/́/g; s/a/́/g; s/e/́/g; s/i/́/g; s/o/́/g; s/u/́/g; s/A/̀/g; s/E/̀/g; s/I/̀/g; s/O/̀/g; s/U/̀/g; s/a/̀/g; s/e/̀/g; s/i/̀/g; s/o/̀/g; s/u/̀/g; '
